e6b1f8d0a295d58004b39ab8c81e87ade532d1aa
LongStrangeTripp <frnkzto@gmail.com>
2024-02-13 20:07:23 -0500

    CSS Styling Attempt
    Made a .css styling file for the create accounts page, I'm not entirely sure if I did it "right" but I made a css class for all the elements on the page one by one.  Probably redundant but if we want to change anything that spans multiple screens then we can start there.

--- src/components/createAccount.js
+++ src/components/createAccount.js
@@ -1,47 +1,44 @@
 import React from 'react';
-import BalanceWizardLogo from "./BalanceWizardLogo.jpg"
+import BalanceWizardLogo from "./BalanceWizardLogo.jpg";
+import './createAccount.css';
 
 export const CreateAccount = () => {
     return (
         <div>
-            {/* Top section */}
-            <div style={{ backgroundColor: 'white', padding: '20px', display: 'flex', alignItems: 'center' }}>
-                <img src={BalanceWizardLogo} alt="logo" style={{ width: '65px', height: '65px', marginRight: '10px', marginLeft: '5px' }} />
-                <div style={{ fontSize: '36px', color: 'black', margin: '0' }}>Balance Wizard</div>
-                <div style={{ fontSize: '24px', color: 'black', marginLeft: 'auto' }}>
-                    <button style={{ marginRight: '10px', marginTop: '5px' }}>New User</button> |
-                    <button style={{ marginLeft: '10px', marginTop: '5px' }}>Login</button>
+            <div className="container">
+                <img src={BalanceWizardLogo} alt="logo" className="logo" />
+                <h1 className="title">Balance Wizard</h1>
+                <div className="buttons">
+                    <button>New User</button> |
+                    <button>Login</button>
                 </div>
             </div>
 
-            {/* Potential Menu Bar */}
             <div style={{ backgroundColor: '#AFABAB', textAlign: 'center', color: 'black', padding: '10px', border: '1px solid black' }}>
-                Potential Menu Bar for Future Functions
+                Menu Bar for Future Functions
             </div>
 
-            {/* Blue Box */}
-            <div style={{ backgroundColor: '#8FAADC', minHeight: 'calc(100vh - 40px)', padding: '20px', border: '1px solid black' }}>
-                {/* Create New User Box */}
-                <div style={{ backgroundColor: '#D9D9D9', padding: '20px', border: '1px solid black', height: '70vh', overflowY: 'auto' }}>
-                    <div style={{ fontSize: '24px', textAlign: 'center', color: 'black', marginBottom: '75px' }}>Create a New User</div>
-                    <form style={{ display: 'flex', flexDirection: 'column' }}>
-                        <div style={{ marginBottom: '75px', display: 'flex', alignItems: 'center' }}>
-                            <label htmlFor="firstName" style={{ color: 'black', width: '150px', marginRight: '10px' }}>First Name:</label>
-                            <input type="text" id="firstName" style={{ width: '300px' }} />
+            <div className="blue-box">
+                <div className="user-box">
+                    <h2 className="user-box-title">Create a New User</h2>
+                    <form className="form-container">
+                        <div className="label-container">
+                            <label htmlFor="firstName" className="label">First Name:</label>
+                            <input type="text" id="firstName" className="input-field" />
                         </div>
-                        <div style={{ marginBottom: '75px', display: 'flex', alignItems: 'center' }}>
-                            <label htmlFor="lastName" style={{ color: 'black', width: '150px', marginRight: '10px' }}>Last Name:</label>
-                            <input type="text" id="lastName" style={{ width: '300px' }} />
+                        <div className="label-container">
+                            <label htmlFor="lastName" className="label">Last Name:</label>
+                            <input type="text" id="lastName" className="input-field" />
                         </div>
-                        <div style={{ marginBottom: '75px', display: 'flex', alignItems: 'center' }}>
-                            <label htmlFor="address" style={{ color: 'black', width: '150px', marginRight: '10px' }}>Address:</label>
-                            <input type="text" id="address" style={{ width: '300px' }} />
+                        <div className="label-container">
+                            <label htmlFor="address" className="label">Address:</label>
+                            <input type="text" id="address" className="input-field" />
                         </div>
-                        <div style={{ marginBottom: '75px', display: 'flex', alignItems: 'center' }}>
-                            <label htmlFor="dob" style={{ color: 'black', width: '150px', marginRight: '10px' }}>Date of Birth:</label>
-                            <input type="text" id="dob" style={{ width: '300px' }} />
+                        <div className="label-container">
+                            <label htmlFor="dob" className="label">Date of Birth:</label>
+                            <input type="text" id="dob" className="input-field" />
                         </div>
-                        <div style={{ textAlign: 'center' }}>
+                        <div className="submit-button">
                             <button type="submit">Submit</button>
                         </div>
                     </form>
