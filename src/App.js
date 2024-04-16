5f31b653f760605ed64add6e804f922d0a6f7fab
LongStrangeTripp <frnkzto@gmail.com>
2024-04-16 19:52:39 -0400

    Adding ledger page button

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { UserProvider } from "./components/userContext";
 import { ForgetPassword } from './components/forgetPassword';
 import Email from './components/SendEmail/SendEmailPage';
 import StatementPage from "./components/statementPage";
+import ledgerPage from "./components/ledgerPage";
 
 function App () {
   return (
@@ -27,6 +28,7 @@ function App () {
                 <Route path="/send-email" element={<Email />} />
                 <Route path="/journal" element={<JournalPage />} />
                 <Route path="/statements" element={<StatementPage />} />
+                <Route path="/ledger" element={<ledgerPage />} />
                 {/* Add more routes for other pages as needed */}
             </Routes>
         </Router>
