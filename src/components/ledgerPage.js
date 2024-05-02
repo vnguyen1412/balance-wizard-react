42f0762c83e5f66fe8badb3503cf96ddbc4a3465
JoshuaBoswell02 <joshuaboswell1234@gmail.com>
2024-05-02 19:31:51 -0400

    commit

--- src/components/ledgerPage.js
+++ src/components/ledgerPage.js
@@ -175,7 +175,7 @@ const LedgerPage = () => {
                                             <tr key={dataIndex}>
                                                 <td>{dataValue}</td>
                                                 <td>{account.listOfExplainations[dataIndex]}</td>
-                                                <td>{account.listOfJournalRefs[dataIndex]}</td>
+                                                <td><Link to={`/journal/${account.listOfJournalRefs[dataIndex]}`}>{account.listOfJournalRefs[dataIndex]}</Link></td>
                                                 {/* this is a conditional rendering to make sure that the amounts are recorded in the correct columns for debit and credit */}
                                                 {account.listOfAmountType[dataIndex] === "debit" ? (
                                                     <>
