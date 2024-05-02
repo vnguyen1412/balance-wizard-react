e3f70b6e6b56b602dadfc902fd8f65ff253a8a67
JoshuaBoswell02 <joshuaboswell1234@gmail.com>
2024-05-02 16:08:44 -0400

    Update LedgerPage.js

--- src/components/LedgerPage.js
+++ src/components/LedgerPage.js
@@ -116,7 +116,6 @@ const LedgerPage = () => {
                                 <Link to="/journal"><button className='menuBarButtons'>Journals</button></Link>
                                 <Link to="/ledger-page"><button className='menuBarButtons'>Ledgers</button></Link>
                                 <Link to="/statements"><button className='menuBarButtons'>Statements</button></Link>
-                                <Link to="/help"><button className='menuBarButtons'>Help</button></Link>
                             </>
                         )}
                         {(user.role === 'Manager' || user.role === 'Administrator') && (
@@ -127,7 +126,6 @@ const LedgerPage = () => {
                                 <Link to="/journal"><button className='menuBarButtons'>Journals</button></Link>
                                 <Link to="/ledger-page"><button className='menuBarButtons'>Ledgers</button></Link>
                                 <Link to="/statements"><button className='menuBarButtons'>Statements</button></Link>
-                                <Link to="/help"><button className='menuBarButtons'>Help</button></Link>
                             </>
                         )}
                     </>
